3eea9dcf29202cdf890655916c84cc2917f033f7
FahmiEfendy <fahmi.efendy@binus.ac.id>
2022-11-29 20:09:03 +0700

    feat add homepage

---
+++ src/components/EventBar.jsx
@@ -0,0 +1,37 @@
+const styles = {
+  eventOrganizerLogo: {
+    height: "40px",
+    width: "60px",
+    backgroundColor: "white",
+  },
+};
+
+const EventBar = () => {
+  return (
+    <div className="row w-100 d-flex align-items-center">
+      <div className="col-3 d-flex">
+        <img
+          style={styles.eventOrganizerLogo}
+          className="rounded my-auto"
+          src={require("../assets/logo-binus.png")}
+          alt="Example Event Organizer Logo"
+        />
+        <div className="mx-3">
+          <p className="my-1">Example Event Name</p>
+          <p className="my-1">Example Event Organizer</p>
+        </div>
+      </div>
+      <div className="col-2">Example Event Date</div>
+      <div className="col-2">Example Event Location</div>
+      <div className="col-2">Example Event Participant</div>
+      <div className="col-2">Example Event Price</div>
+      <div className="col-1 d-flex">
+        <button type="button" className="btn btn-primary px-4 mx-auto">
+          Detail
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default EventBar;
