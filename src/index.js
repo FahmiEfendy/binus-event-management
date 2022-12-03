2365e8d3c1165547252cb65af12cbeb757f38ebc
Fahmi Efendy <itsfahmiefendy@gmail.com>
2022-12-03 12:07:56 +0700

    fix route when start website

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/binus-event-management">
       <App />
     </Router>
   </React.StrictMode>
