4861d32cad7da01c4dfbf35de12c50ab046758ff
Fahmi <itsfahmiefendy@gmail.com>
2023-01-19 15:47:08 +0700

    feat: implement rtk query for HTTP Request

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
 
 import "./index.css";
 import App from "./App";
+import { api } from "./api/api";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router basename="/binus-event-management">
-      <App />
-    </Router>
+    <ApiProvider api={api}>
+      <Router basename="/binus-event-management">
+        <App />
+      </Router>
+    </ApiProvider>
   </React.StrictMode>
 );
