c6f14334d35810278a15bcd051c500f2737d2105
Fahmi <itsfahmiefendy@gmail.com>
2023-02-15 12:10:25 +0700

    fix: get enrolled event

--- src/components/pages/EventHistory.jsx
+++ src/components/pages/EventHistory.jsx
@@ -1,27 +1,9 @@
-import { useEffect, useState } from "react";
-
 import { EventList } from "../molecules";
 import { getToken } from "../../utils/storage";
 import { useGetEnrolledEventQuery } from "../../api/eventApi";
 
 const EventHistory = ({ searchValue }) => {
-  const [responseMessage, setResponseMessage] = useState("");
-
-  const {
-    data: enrolledEventList,
-    isSuccess,
-    isError,
-  } = useGetEnrolledEventQuery(getToken());
-
-  useEffect(() => {
-    if (isSuccess) {
-      setResponseMessage("Success Get Enrolled Event List");
-    } else if (isError) {
-      setResponseMessage("Failed Get Enrolled Event List");
-    }
-
-    console.log(responseMessage);
-  }, [enrolledEventList, isError, isSuccess, responseMessage]);
+  const { data: enrolledEventList } = useGetEnrolledEventQuery(getToken());
 
   return (
     <EventList
